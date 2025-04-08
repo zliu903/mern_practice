f58d8d731493f9c9bf55b87ad002e92328e5f43f
lzy <zliu903@aucklanduni.ac.nz>
2025-04-08 19:06:55 +1200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("api/profile", (req, res) => {
+  res.json({
+    name: "John Doe",
+    skills: ["JavaScript", "Node.js", "Express", "MongoDB"],
+  });
+});
+
+app.get("api/projects", (req, res) => {
+  res.json([
+    { name: "portfolio website", description: "My personal portfolio website" },
+    {
+      name: "todo app",
+      description: "A simple todo list app built with Node.js",
+    },
+  ]);
+});
+
+app.listen(PORT, () => {
+    console.log(`sever is running on http://localhost:${PORT}`);
+});
